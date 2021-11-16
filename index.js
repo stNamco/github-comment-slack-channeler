4b210781481429eb4a780932e8837f0aa5a4b0fc
stnamco <lbnamco15@gmail.com>
2021-11-16 22:48:10 +0900

    change to js

---
+++ index.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+} catch (error) {
+  core.setFailed(error.message);
+}
