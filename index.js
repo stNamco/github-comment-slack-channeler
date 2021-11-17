201b1403d664164320d0c0d5df49d4d0dcbf90fe
stnamco <lbnamco15@gmail.com>
2021-11-18 08:26:18 +0900

    change to js

--- index.js
+++ index.js
@@ -1,91 +1,96 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { WebClient, ErrorCode } = require('@slack/web-api');
+const { WebClient } = require('@slack/web-api');
 
-// async function fetch() {
-//   const myToken = core.getInput('github_token');
-//   const octokit = github.getOctokit(myToken);
+async function createUnfurl(url) {
+  const githubToken = core.getInput('github_token')
 
-//   try {
-//     // `who-to-greet` input defined in action metadata file
-//     // const  = core.getInput('who-to-greet');
-//     // console.log(`Hello ${nameToGreet}!`);
-//     const { data: pullRequest } = await octokit.rest.pulls.get({
-//         owner: 'stNamco',
-//         repo: 'github-comment-slack-channeler',
-//         pull_number: 5
-//     });
-//     console.log(pullRequest);
-    
-//   } catch (error) {
-//     core.setFailed(error.message);
-//   }
-// }
-
-async function createUnfurl() {
   function parseUrl(url) {
-    values = url.split("#")
-    issue_id = values[0].split("/")[-1]
-    params = {
+    const values = url.split("#")
+    const issueId = values[0].split("/")[-1]
+    let params = {
       "owner": url.split("/")[3],
       "repoName": url.split("/")[4],
-      "issue_id": issue_id
+      "issueId": issueId
       }
     
     if (values[1].includes("issuecomment")) {
-      q_params = values[1].split("-")
-      params["type"] = q_params[0]
-      params["type_id"] = q_params[1]
+      const qParams = values[1].split("-")
+      params["type"] = qParams[0]
+      params["typeId"] = qParams[1]
     } else if (values[1].includes("discussion")) {
-      q_params = values[1].split("_")
-      params["type"] = q_params[0]
-      params["type_id"] = q_params[1].replace('r', '')
+      const qParams = values[1].split("_")
+      params["type"] = qParams[0]
+      params["typeId"] = qParams[1].replace('r', '')
     }
     return params
   };
 
-  async function fetch_comment(owner, repoName, type, issue_id, type_id) {
-    token = core.getInput('github_token')
-    const octokit = github.getOctokit(token);
+  async function fetchComment(owner, repoName, type, typeId) {
+    const octokit = github.getOctokit(githubToken);
 
     if (type == "issuecomment") {
       return await octokit.request('GET /repos/{owner}/{repo}/issues/comments/{comment_id}', {
         owner: owner,
         repo: repoName,
-        comment_id: type_id
+        comment_id: typeId
       })
     } else if(type == "discussion") {
       return await octokit.request('GET /repos/{owner}/{repo}/pulls/comments/{comment_id}', {
         owner: owner,
         repo: repoName,
-        comment_id: type_id
+        comment_id: typeId
       })
+    } else {
+      throw new Error('error');
     }
   }
 
-  url = core.getInput('github_comment_url')
-  params = parseUrl(url)
-  comment = await fetch_comment(params["owner"],  params["repoName"], params["type"], params["issue_id"], params["type_id"])
+  const params = parseUrl(url)
+  const res = await fetchComment(params.owner,  params.repoName, params.type, params.typeId).catch(e => console.error(e));
+  const comment = res.data
 
-  console.log(comment)
-
-  // jst = timezone(timedelta(hours=+9), 'JST')
-  // datetime_jst = comment.created_at.astimezone(jst)
-
-  time = new Date(comment.created_at);
-  time.setHours(time.getHours() + 9);
-  datetime_jst = time.toLocaleString().slice(0,-3);
-
-  info = {
-      "title": params["type"],
+  const info = {
+      "title": params.type,
       "title_link": url,
       "author_name": comment.user.login,
       "author_icon": comment.user.avatar_url,
       "text": comment.body,
-      "ts": datetime_jst 
+      "ts": new Date(comment.created_at).getTime()
   }
-
   return info
 }
 
-console.log(createUnfurl())
+class SlackService {
+
+  // NOTE: https://slack.dev/node-slack-sdk/web-api
+
+  static get client() {
+    return new WebClient(core.getInput('slack_bot_token'))
+  }
+
+  static async callPostChat(channelId, url) {
+    return await SlackService.client.chat.postMessage({
+      text: url,
+      channel: channelId,
+    });
+  }
+
+  static async callUnfurl(channelId, unfurls, ts) {
+    return await SlackService.client.chat.unfurl({
+      channel: channelId,
+      ts: ts,
+      unfurls: unfurls
+    })
+  }
+}
+
+(async () => {
+  const githubCommentUrl = core.getInput('github_comment_url')
+  const slackChannelId = core.getInput('slack_channel_id')
+
+  const resPostChat = await SlackService.callPostChat(slackChannelId, githubCommentUrl).catch(e => console.error(e));
+  const unfurl = await createUnfurl(githubCommentUrl).catch(e => console.error(e));
+  const unfurls = {[githubCommentUrl]: unfurl}
+  await SlackService.callUnfurl(slackChannelId, unfurls, resPostChat.message.ts).catch(e => console.error(e));
+})();
