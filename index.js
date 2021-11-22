fae253128416e87cf83bf20b9ac5dcc9f7e94963
stnamco <stNamco@users.noreply.github.com>
2021-11-23 06:32:39 +0900

    Update index.js
    - xxx/files#r\d{9}のパターンもparseできる。
    - bodyに含まれるgithub commentのurlをparseできる
    - key, channelの組み合わせを複数登録できる。

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { WebClient } = require('@slack/web-api');
 
-async function createBlocks(url, targetLabel) {
-
-  const githubToken = core.getInput('github_token')
+async function createBlocks(githubToken, config, url, unfurlLinksContextStr) {
 
   function parseUrl(url) {
     const values = url.split("#")
-    const issueId = values[0].split("/")[-1]
+    const urlParams = values[0].split("/")
     let params = {
-      "owner": url.split("/")[3],
-      "repoName": url.split("/")[4],
-      "issueId": issueId
+      "owner": urlParams[3],
+      "repoName": urlParams[4]
       }
     
     if (values[1].includes("issuecomment")) {
       const qParams = values[1].split("-")
       params["type"] = qParams[0]
       params["typeId"] = qParams[1]
+    } else if (values[0].includes("files")) {
+      params["type"] = urlParams[urlParams.length - 1]
+      params["typeId"] = values[1].replace('r', '')
     } else if (values[1].includes("discussion")) {
       const qParams = values[1].split("_")
       params["type"] = qParams[0]
@@ -26,12 +26,18 @@ async function createBlocks(url, targetLabel) {
     }
     return params
   };
-
-  async function fetchComment(owner, repoName, type, typeId) {
+  
+  async function fetchComment(githubToken, owner, repoName, type, typeId) {
     const octokit = github.getOctokit(githubToken);
-
+  
     if (type == "issuecomment") {
       return await octokit.request('GET /repos/{owner}/{repo}/issues/comments/{comment_id}', {
+        owner: owner,
+        repo: repoName,
+        comment_id: typeId
+      })
+    } else if(type == "files") {
+      return await octokit.request('GET /repos/{owner}/{repo}/pulls/comments/{comment_id}', {
         owner: owner,
         repo: repoName,
         comment_id: typeId
@@ -46,7 +52,7 @@ async function createBlocks(url, targetLabel) {
       throw new Error('error');
     }
   }
-
+  
   function formatDate(dateString) {
     date = new Date(dateString)
     var y = date.getFullYear();
@@ -55,62 +61,112 @@ async function createBlocks(url, targetLabel) {
     return (y + '/' + m + '/' + d);
   }
 
-  const params = parseUrl(url)
-  const res = await fetchComment(params.owner,  params.repoName, params.type, params.typeId).catch(e => console.error(e));
-  const comment = res.data
-
-  return new Promise(function(resolve, reject) {  
-
-    if (!comment.body.includes(targetLabel)) {
-      reject("target label is not included")
+  async function unfurlIfNeeded(unfurlLinksContextStr, githubToken, body) {
+    if (unfurlLinksContextStr === 'false') {
+      return []
     }
 
-    const body = comment.body.startsWith(targetLabel) ? comment.body.slice(targetLabel.length) : comment.body;
+    let urls = body.match(/https:\/\/github.com[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+(discussion_r\d{9}|issuecomment-\d{9}|files#r\d{9})/g);
+    if (urls === null) {
+      return []
+    }
+    
+    let unfurlBody = "\n";
+    for (let i = 0; i < urls.length; i++) {
+      const params = parseUrl(urls[i]);
+      const res = await fetchComment(githubToken, params.owner,  params.repoName, params.type, params.typeId).catch(e => console.error(e));
+      unfurlBody += `\n\nAuthor: ${res.data.user.login} \nComment: ${res.data.body}`
+    }
 
-    const blocks = [{
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": `*質問や共有内容が追加されました！by ${comment.user.login}*`
-          }
+    return [
+      {
+        'type': 'divider'
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": unfurlBody
+        }
+      },
+      {
+        'type': 'divider'
+      }
+    ]
+  }
+  
+  function detectTargetConfig(config) {
+    const keys = Object.keys(config)
+    for (let i = 0; i < keys.length; i++) {
+      if (comment.body.startsWith(keys[i])) {
+        return {key: keys[i], channelId: config[keys[i]]}
+      }
+    }
+    throw new Error('there is no detected value');
+  };
+
+  const params = parseUrl(url)
+  const res = await fetchComment(githubToken, params.owner,  params.repoName, params.type, params.typeId).catch(e => console.error(e));
+  const comment = res.data
+
+  let targetConfig = {}
+  try {
+    targetConfig = detectTargetConfig(config)
+  } catch (error) {
+    throw new Error(error);
+  }
+
+  let body = comment.body.startsWith(targetConfig.key) ? comment.body.slice(targetConfig.key.length) : comment.body;
+  let blocks = [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `*質問や共有内容が追加されました！by ${comment.user.login}*`
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `${body} \n\n <${url}|${params.owner}/${params.repoName}> | ${formatDate(comment.created_at)}`
         },
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": `${body} \n\n <${url}|${params.owner}/${params.repoName}> | ${formatDate(comment.created_at)}`
-          },
-          "accessory": {
-            "type": "image",
-            "image_url": comment.user.avatar_url,
-            "alt_text": "avatar image"
-          }
-        }]
-    resolve(blocks)
-  });  
+        "accessory": {
+          "type": "image",
+          "image_url": comment.user.avatar_url,
+          "alt_text": "avatar image"
+        }
+      }
+  ]
+
+  blocks = blocks.concat(await unfurlIfNeeded(unfurlLinksContextStr, githubToken, body));
+
+  return [targetConfig.channelId, blocks]
 }
 class SlackService {
-
   // NOTE: https://slack.dev/node-slack-sdk/web-api
-
   static async callPostChat(channelId, blocks) {
     const client = new WebClient(core.getInput('slack_bot_token'))
     return await client.chat.postMessage({
       text: "質問や共有内容が追加されました!",
       channel: channelId,
-      blocks: blocks
+      blocks: blocks,
+      unfurl_links: true
     });
   }
 }
 
 (async () => {
-  const githubCommentUrl = core.getInput('github_comment_url')
-  const slackChannelId = core.getInput('slack_channel_id')
-  const targetLabel = core.getInput('target_label')
+  const githubToken = core.getInput('github_token')
+  const githubCommentUrl = core.getInput('github_comment_url');
+  const channelerTargetCongfig = core.getInput('channeler_target_config');
+  const unfurlLinksContextStr = core.getInput('unfurl_links_context')
 
-  createBlocks(githubCommentUrl, targetLabel).then(blocks => {
-    SlackService.callPostChat(slackChannelId, blocks).catch(e => console.error(e));
-  }, error => {
-    console.log(error)
-  });
+  try {
+    const config = JSON.parse(channelerTargetCongfig);
+    const [channelId, blocks] = await createBlocks(githubToken, config, githubCommentUrl, unfurlLinksContextStr).catch(e => console.error(e));
+    SlackService.callPostChat(channelId, blocks).catch(e => console.error(e));
+  } catch (error) {
+    console.log(error);
+  }
 })();
